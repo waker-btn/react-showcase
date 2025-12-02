121054262e82125a135338ce58f30b4d635e1a35
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-12-02 11:18:04 +0000

    CSS for Tabs and Viewer components

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -1,9 +1,20 @@
+import styles from '@/components/Tabs/Tab.module.css'
+
 interface TabProps {
   label: string
+  isActive?: boolean
+  onClick: () => void
 }
 
-function Tab({ label }: TabProps) {
-  return <>{label}</>
+function Tab({ label, isActive, onClick }: TabProps) {
+  return (
+    <div
+      className={isActive ? styles['tab--selected'] : styles.tab}
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  )
 }
 
 export default Tab
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,14 +1,23 @@
 import Tab from '@/components/Tabs/Tab'
+import { useState } from 'react'
+import styles from '@/components/Tabs/Tabs.module.css'
 
 const tabLabels: string[] = ['Tab 1', 'Tab 2', 'Tab 3']
 
 function Tabs() {
+  const [activeTab, setActiveTab] = useState<string>(tabLabels[0])
+
   return (
-    <>
+    <div className={styles.tabs}>
       {tabLabels.map((label) => (
-        <Tab key={label} label={label} />
+        <Tab
+          key={label}
+          label={label}
+          isActive={label === activeTab}
+          onClick={() => setActiveTab(label)}
+        />
       ))}
-    </>
+    </div>
   )
 }
 
